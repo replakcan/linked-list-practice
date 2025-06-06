c337b1273921c7590b3e1327b59c50d31e744094
replakcan <alper.makcan@gmail.com>
2025-06-06 15:33:35 +0300

    Joins Node and LinkedList files
    class Node wasn't providing anything with it's alone, I added it to the LinkedList class file.

--- LinkedList.js
+++ LinkedList.js
@@ -1,4 +1,9 @@
-import Node from "./Node.js";
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
 
 class LinkedList {
   constructor(headNode = null) {
